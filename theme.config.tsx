06d1df457af670473caa8afd1b26fe94ff00a28f
imikodanic <imikodanic@gmail.com>
2024-09-06 10:50:23 +0200

    feat: add SEO options to config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,19 +1,59 @@
-import type { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import { type DocsThemeConfig, useConfig } from "nextra-theme-docs";
+
+import seoConfig from "./seo.config";
+import { Icon } from "./theme/icons";
 
 const config: DocsThemeConfig = {
-    logo: <span>Treblle</span>,
+    logo: <Icon icon="LogoPositive" height="28px" width="100px" />,
     project: {
-        link: "https://github.com/shuding/nextra-docs-template",
+        link: "https://github.com/Treblle/treblle-docs-nextra",
     },
     chat: {
-        link: "https://discord.com",
+        link: "https://discord.com/invite/hgt78mXwzv",
     },
-    docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+    docsRepositoryBase: "https://github.com/Treblle/treblle-docs-nextra",
     footer: {
-        text: "Treblle",
+        text: "©Treblle. All Rights Reserved.",
     },
     nextThemes: {
         defaultTheme: "system",
+    },
+    themeSwitch: {},
+    useNextSeoProps() {
+        const { route } = useRouter();
+        const { url, images } = seoConfig.openGraph;
+
+        if (route === "/") {
+            return { titleTemplate: "%s - Treblle" };
+        }
+
+        return {
+            titleTemplate: seoConfig.title.template,
+            openGraph: { url, images: [{ url: `${url}${images}` }] },
+        };
+    },
+    head: () => {
+        const { frontMatter: meta } = useConfig();
+        const { title } = meta;
+
+        return (
+            <>
+                <meta httpEquiv="Content-Language" content="en" />
+                <meta name="description" content={meta.description || seoConfig.description} />
+                <meta
+                    name="og:title"
+                    content={title ? title + " – Treblle" : seoConfig.title.default}
+                />
+                <meta name="og:description" content={meta.description || seoConfig.description} />
+                <meta name="og:image" content={seoConfig.openGraph.images} />
+                <meta name="og:url" content={seoConfig.openGraph.url} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content={seoConfig.twitter.site} />
+                <meta name="twitter:creator" content={seoConfig.twitter.creator} />
+                <meta name="apple-mobile-web-app-title" content="Treblle" />
+            </>
+        );
     },
 };
 
